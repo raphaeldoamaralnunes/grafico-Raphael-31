0a4077e3604bd04da46ff609fb195995e5b2456c
raphaeldoamaralnunes <162448937+raphaeldoamaralnunes@users.noreply.github.com>
2024-10-30 09:05:52 -0300

    Update script.js

--- grafico-raphael-31/script.js
+++ grafico-raphael-31/script.js
@@ -1,7 +1,7 @@
-const grafico = document.getElementById('myChart').getContext("2d");
-var meuGrafico = new Chart(grafico,{
-    type:"pie",
-    date: {
+const grafico = document.getElementById("meuGrafico").getContext("2d");
+const meuGrafico = new Chart(grafico, {
+    type: "pie",
+    data: {
         labels: [
             "Instagram",
             "WhatsApp",
@@ -13,7 +13,34 @@ var meuGrafico = new Chart(grafico,{
             "Telegram",
             "FB Messenger",
             "LINE"
-        ]
+        ],
+        datasets: [{
+            data:[
+                16.5,
+                16.1,
+                12.8,
+                12.8,
+                7.4,
+                6.6,
+                3.2,
+                2.4,
+                2.3,
+                1.7
+            ],
+            backgroundColor: [
+                "#14E332",
+                "#E3AC14",
+                "#2B392D",
+                "#2B392D",
+                "#3B4463",
+                "#8E3C50",
+                "#8E783C",
+                "#1441E3",
+                "#E31447",
+                "#374FA3",
+                "#3C8E48"
+            ]
+        }]
     }
 }
 )
